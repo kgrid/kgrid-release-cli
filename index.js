cf5e0560975aa54e3e0a849bb4cfb8920988db36
gregfarris <farrisg@umich.edu>
2019-03-27 15:36:59 -0400

    First commit of release cli

---
+++ index.js
@@ -0,0 +1,159 @@
+#!/usr/bin/env node
+
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const axios = require('axios');
+const requireEnv = require("require-environment-variables");
+const _ = require('lodash');
+
+requireEnv(['CIRCLECI_TOKEN']);
+
+const init = () => {
+
+  console.log(
+      chalk.blue.bold(
+          figlet.textSync("KGrid Release CLI", {
+            horizontalLayout: "default",
+            verticalLayout: "default"
+          })
+      )
+  );
+
+}
+
+const askQuestions = () => {
+  const questions = [
+    {
+      name: "component",
+      type: "list",
+      message: "What KGrid Component are you Releasing?",
+      choices: [
+        new inquirer.Separator("--- Components ---"),
+        {
+          name: 'Activator',
+          value: 'kgrid/kgrid-activator'
+        },
+        {
+          name: 'Adapter',
+          value: 'kgrid/kgrid-adapter'
+        },
+        {
+          name: 'Library',
+          value: 'kgrid/kgrid-library'
+        },
+        {
+          name: 'Play',
+          value: 'kgrid/kgrid-play'
+        },
+        {
+          name: 'Shelf',
+          value: 'kgrid/kgrid-shelf'
+        },
+        new inquirer.Separator("--- KO Collections ---"),
+        {
+          name: 'IPP Collection',
+          value: 'kgrid-objects/ipp-collection'
+        },
+        {
+          name: 'Example Collection',
+          value: 'kgrid-objects/example-collection'
+        },
+        {
+          name: 'Opioid Collection',
+          value: 'kgrid-objects/opioid-collection'
+        },
+        new inquirer.Separator("--- Demo Clients ---"),
+        {
+          name: 'Cancer Advisor Demo',
+          value: 'kgrid-demos/cancer-advisor'
+        },
+        {
+          name: 'Labwise Demo',
+          value: 'kgrid-demos/labwise'
+        },
+        {
+          name: 'RxPattern Demo',
+          value: 'kgrid-demos/rxpattern'
+        },
+        {
+          name: 'Cardiac Advisor aka postpci Demo',
+          value: 'kgrid-demos/cardiac-advisor'
+        },
+        {
+          name: 'Drug Gene Tool Demo',
+          value: 'kgrid-demos/druggenetool'
+        },
+        {
+          name: 'CPIC Kit Demo',
+          value: 'kgrid-demos/cpic-kit'
+        },
+        {
+          name: 'KGrid Kit Demo',
+          value: 'kgrid-demos/kgrid-kit'
+        },
+
+      ],
+      filter: function(val) {
+        return val.toLowerCase();
+      }
+    },
+    {
+      type: "input",
+      name: "branch",
+      message: "What branch (e.g. hotfix, feature or master)?",
+      default:"master"
+    },
+    {
+      type: "input",
+      name: "module",
+      message: "What is the Component Module (e.g. adapter-api or javascript-adapter or blank)?"
+    },
+    {
+      type: "input",
+      name: "releaseversion",
+      message: "What is the Release Version (e.g. 1.0.3)?"
+    },
+    {
+      type: "input",
+      name: "deploymentversion",
+      message: "What is the Development Version (e.g 1.0.4-SNAPSHOT)?"
+    },
+    {
+      type: 'confirm',
+      name: 'prerelease',
+      message: 'Is this PreRelease?',
+      default: false
+    }
+  ];
+  return inquirer.prompt(questions);
+};
+
+const run = async () => {
+  // show script introduction
+  init();
+
+  const answers = await askQuestions();
+
+  let url = "https://circleci.com/api/v1.1/project/github/"+ answers.component + "/tree/" + answers.branch +"?circle-token="+process.env.CIRCLECI_TOKEN;
+  let data = {"build_parameters":{
+      "RELEASE":answers.releaseversion,
+      "NEXT":answers.deploymentversion,
+      "PRERELEASE": answers.prerelease,
+      "MODULE":answers.module,
+      "GIT_USER_NAME": "KGridReleaseCLI",
+      "GIT_USER_EMAIL":"kgrid-developers@umich.edu"}};
+
+  axios.post(url, data)
+  .then(function (response) {
+    // handle success
+    console.log("Build has started" );
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
+};
+
+run();
---
+++ readme.md
@@ -0,0 +1,66 @@
+# KGrid Release CLI
+
+This CLI will help you trigger a Release of the KGrid components.  
+
+## Getting Started
+
+1. Get a Circle CI a [personal api token](https://circleci.com/docs/2.0/managing-api-tokens/#creating-a-personal-api-token)
+1.  ```npm install -g  @kgrid/release-cli ```
+1. Run ```export CIRCLECI_TOKEN=#### && kgrid-release``` 
+
+## Using the CLI
+ 
+1. Choose the component
+1. Adapter is sub module so if Adapter you have to say api or javascript otherwise just press enter
+1. The release version  #.#.# for releases #.#.#-rc# for release candidates. For maven projects this sets
+the version and will used to tag the GitHub repository
+1. The development version will be #.#.#-SNAPSHOT
+1. If it is Pre Release type _Y_ defaults to _N_
+
+Review the additional [#note]
+
+Will kick off Circle CI build.
+
+```text
+  _  ______      _     _   ____      _                        ____ _     ___ 
+ | |/ / ___|_ __(_) __| | |  _ \ ___| | ___  __ _ ___  ___   / ___| |   |_ _|
+ | ' / |  _| '__| |/ _` | | |_) / _ \ |/ _ \/ _` / __|/ _ \ | |   | |    | | 
+ | . \ |_| | |  | | (_| | |  _ <  __/ |  __/ (_| \__ \  __/ | |___| |___ | | 
+ |_|\_\____|_|  |_|\__,_| |_| \_\___|_|\___|\__,_|___/\___|  \____|_____|___|
+                                                                             
+? What KGrid Component are you Releasing? (Use arrow keys)
+❯ Activator 
+  Adapter 
+  Library 
+  Play 
+  Shelf 
+```
+
+
+Here is an example of final screen
+
+```text
+  _  ______      _     _   ____      _                        ____ _     ___ 
+ | |/ / ___|_ __(_) __| | |  _ \ ___| | ___  __ _ ___  ___   / ___| |   |_ _|
+ | ' / |  _| '__| |/ _` | | |_) / _ \ |/ _ \/ _` / __|/ _ \ | |   | |    | | 
+ | . \ |_| | |  | | (_| | |  _ <  __/ |  __/ (_| \__ \  __/ | |___| |___ | | 
+ |_|\_\____|_|  |_|\__,_| |_| \_\___|_|\___|\__,_|___/\___|  \____|_____|___|
+                                                                             
+? What KGrid Component are you Releasing? Activator
+? What branch (e.g. hotfix, feature or master)? master
+? What is the Component Module (e.g. adapter-api or javascript-adapter or blank)? 
+? What is the Release Version (e.g. 1.0.3)? 1.0.1-rc3
+? What is the Development Version (e.g 1.0.4-SNAPSHOT)? 1.0.1-SNAPSHOT
+? Is this PreRelease? Yes
+Build has started
+~/kgrid-config/release/cli $ 
+
+```
+
+## Notes
+
+- Component Module is only need when releasing Adapters.  The project consists of mmaven submodules 
+and thus requires more information
+
+- The Demo components release process consists of updateing the the GitHub pages.  When releasing Demos
+you really only have to input a Release Version, it will be used to tag the repo.
